1fae446e2c0d8d5205ffdd44e1c812b10018b3c4
artemprivalov <privalov.dev@gmail.com>
2019-12-12 02:20:33 +0400

    Tak 2

---
+++ frontend/config/webpack.prod.js
@@ -0,0 +1,48 @@
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+const {resolvePath} = require('./utils');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[name].[hash].js',
+        path: resolvePath('build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            title: 'Shelter',
+            template: './public/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
+        }),
+    ],
+});
