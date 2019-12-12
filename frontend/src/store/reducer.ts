c1dcb64483d98059280bd265df9d32628825c54c
shaml <shamlopeka@gmail.com>
2019-12-12 22:35:21 +0400

    Рабочий вариант (Почти)

--- frontend/src/store/reducer.ts
+++ frontend/src/store/reducer.ts
@@ -4,10 +4,18 @@ import {alertsReducer} from '@store/alerts';
 import {authenticationReducer} from '@store/authentication';
 import {registrationReducer} from '@store/registration';
 import {doctorsReducer} from '@store/doctors';
+import {medicinesReducer} from '@store/medicines';
+import {personsReducer} from '@store/persons';
+import {patientsReducer} from '@store/patients';
+import {prescriptionsReducer} from '@store/prescriptions';
 
 export const reducer: Reducer<RootState> = combineReducers<RootState>({
     alert: alertsReducer,
     auth: authenticationReducer,
     registration: registrationReducer,
     doctors: doctorsReducer,
+    medicines: medicinesReducer,
+    persons: personsReducer,
+    patients: patientsReducer,
+    prescriptions: prescriptionsReducer,
 });
